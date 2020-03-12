b5bf4fda6b4e65edf8ee8f3bf7d9b37b8e9513c6
Innolize <Innolize@users.noreply.github.com>
2020-03-12 04:21:46 -0300

    Separado en modulos y queda funcional
    Separado en mostrar carta, mostrar lista, index y utilidades

--- src/main.js
+++ src/main.js
@@ -15,31 +15,112 @@ async function mostrarListaPokemon(URL) {
         $("#data").append($(`<tr><td class="border px-4 py-2 cursor-pointer" data-pokemon="${pokemon}">${primerLetraMayus(pokemon)}</td><td class="border px-4 py-2">${numeroPokemonLista}</td>`))
 
     })
-    $("tr").click(() => {
-        let click = clickPokemonLista();
-        mostrarPokemonSeleccionado(click);
-    });
     siguienteURL = rJSON.next
     anteriorURL = rJSON.previous
+}
+
+///////////////////////////////////////////////
+
+async function mostrarPokemonSeleccionado(pokemon) {
+    const URL = (`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+    const r = await fetch(URL)
+    const rJSON = await r.json()
+
+    mostrarMiniDescripcion(rJSON)
+    mostrarNumeroYNombrePokemon(rJSON)
+    mostrarImagenPokemon(rJSON)
+    mostrarStatsPokemon(rJSON)
+    obtenerDescripcion(rJSON)
+    mostrarTipo(rJSON)
+    manejarHabilidades(rJSON)
 
 
 }
 
-function primerLetraMayus(palabra) {
-    return palabra.charAt(0).toUpperCase() + palabra.slice(1);
+async function manejarHabilidades(rJSON) {
+    const habilidades = await obtenerHabilidad(rJSON)
+    const habilidadesEspaniol = await traducirEspaniol(habilidades)
+    console.log(habilidadesEspaniol)
+    mostrarHabilidad(habilidadesEspaniol)
 }
 
-
-function obtenerNumeroPokemon(elemento) {
-    let numeroPokemon = elemento.substr(elemento.length - 5)
-    return numeroPokemon.replace(/[^0-9]/g, '');
+function mostrarTipo(rJSON) {
+    debugger
+    console.log(rJSON)
+    $("#tipos").html("")
+    rJSON.types.forEach(types => {
+        $("#tipos").append(`<img src="img/Tipos/${types.type.name}.gif"/>`)
+    })
 }
 
-function clickPokemonLista() {
-    const click = event.target
-    return click.dataset.pokemon
+function mostrarHabilidad(array) {
+    console.log(array)
+    $("#habilidad").html("")
+
+    array.forEach(elemento => {
+        console.log(elemento)
+        $("#habilidad").append(`<div>${elemento}`)
+    })
 }
 
+async function mostrarMiniDescripcion(elemento) {
+    const r = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${elemento.id}/`)
+    const rJSON = await r.json()
+    let miniDescripcion = rJSON.genera.find((x) =>
+        x.language.name === "es");
+    $("#mini-descripcion").text(miniDescripcion.genus)
+}
+
+function mostrarNumeroYNombrePokemon(rJSON) {
+    $("#id-pokemon").text(`Nº ${rJSON.id} ${primerLetraMayus(rJSON.name)}`)
+}
+
+function obtenerImagenPokemon(id) {
+    const URLFoto = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${agregarCeros(id, 3)}.png`
+    return URLFoto;
+}
+
+function mostrarImagenPokemon(rJSON) {
+    $("#imagen-pokemon").attr("src", `${obtenerImagenPokemon(rJSON.id)}`)
+}
+
+function mostrarStatsPokemon(rJSON) {
+    $("#ataque").text(`Ataque: ${rJSON.stats[4].base_stat}`)
+    $("#defensa").text(`Defensa: ${rJSON.stats[3].base_stat}`)
+    $("#ataque-s").text(`Ataque S: ${rJSON.stats[2].base_stat}`)
+    $("#defensa-s").text(`Defensa S: ${rJSON.stats[1].base_stat}`)
+    $("#velocidad").text(`Velocidad: ${rJSON.stats[0].base_stat}`)
+    $("#vitalidad").text(`Vitalidad: ${rJSON.stats[5].base_stat}`)
+}
+
+async function obtenerDescripcion(elemento) {
+    const r = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${elemento.id}/`)
+    const rJSON = await r.json()
+    const descripcion = rJSON.flavor_text_entries.find((x) =>
+        x.language.name === "es");
+    $("#descripcion").text(`${descripcion.flavor_text}`)
+}
+
+async function obtenerHabilidad(elemento) {
+    let array = []
+    const URL = `https://pokeapi.co/api/v2/pokemon/${elemento.name}/`
+    const r = await fetch(URL)
+    const rJSON = await r.json()
+
+    rJSON.abilities.forEach(elemento => {
+        array.push(elemento.ability.url)
+    })
+    console.log(array)
+    return array
+}
+
+/////////////////////////////////////////////////
+
+function buscarPokemon(pokemon) {
+    mostrarPokemonSeleccionado(pokemon)
+}
+
+/////////////////////////////////////////////////
 
 $("#siguiente").click(() => {
     if (siguienteURL === null) {
@@ -59,66 +140,24 @@ $("#anterior").click(() => {
     }
 })
 
-async function mostrarPokemonSeleccionado(pokemon) {
-    const URL = (`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
-    const r = await fetch(URL)
-    const rJSON = await r.json()
-
-    mostrarMiniDescripcion(rJSON.id)
-    mostrarNumeroYNombrePokemon(rJSON)
-    mostrarImagenPokemon(rJSON)
-    mostrarStatsPokemon(rJSON)
-    obtenerDescripcion(rJSON.id)
-    mostrarTipo(rJSON)
-    manejarHabilidades(rJSON)
-    async function manejarHabilidades(rJSON) {
-        const habilidades = await obtenerHabilidad(rJSON.name)
-        const habilidadesEspaniol = await traducirEspaniol(habilidades)
-        console.log(habilidadesEspaniol)
-        mostrarHabilidad(habilidadesEspaniol)
-
-    }
+$("#boton-ingresar-pokemon").click(() => {
+    const pokemon = $("#ingresar-pokemon").val()
+    buscarPokemon(pokemon)
+})
 
 
-}
-function mostrarTipo(rJSON) {
-    debugger
-    console.log(rJSON)
-    $("#tipos").html("")
-    rJSON.types.forEach(types => {
-        $("#tipos").append(`<img src="img/Tipos/${types.type.name}.gif"/>`)
-    })
+
+$("tr").click(() => {
+    let click = clickPokemonLista();
+    mostrarPokemonSeleccionado(click);
+});
+
+function clickPokemonLista() {
+    const click = event.target
+    return click.dataset.pokemon
 }
 
-
-
-
-function mostrarHabilidad(array) {
-    console.log(array)
-    $("#habilidad").html("")
-
-    array.forEach(elemento => {
-        console.log(elemento)
-        $("#habilidad").append(`<div>${elemento}`)
-    })
-}
-
-
-
-
-async function mostrarMiniDescripcion(id) {
-    const r = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}/`)
-    const rJSON = await r.json()
-    let miniDescripcion = rJSON.genera.find((x) =>
-        x.language.name === "es");
-    $("#mini-descripcion").text(miniDescripcion.genus)
-}
-
-
-function obtenerImagenPokemon(id) {
-    const URLFoto = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${agregarCeros(id, 3)}.png`
-    return URLFoto;
-}
+/////////////////////////////////////////
 
 function agregarCeros(numero, longitud) {
     let string = "" + numero;
@@ -128,47 +167,13 @@ function agregarCeros(numero, longitud) {
     return string
 }
 
-function mostrarNumeroYNombrePokemon(rJSON) {
-    $("#id-pokemon").text(`Nº ${rJSON.id} ${primerLetraMayus(rJSON.name)}`)
+function primerLetraMayus(palabra) {
+    return palabra.charAt(0).toUpperCase() + palabra.slice(1);
 }
 
-function mostrarImagenPokemon(rJSON) {
-    $("#imagen-pokemon").attr("src", `${obtenerImagenPokemon(rJSON.id)}`)
-}
-
-function mostrarStatsPokemon(rJSON) {
-    $("#ataque").text(`Ataque: ${rJSON.stats[4].base_stat}`)
-    $("#defensa").text(`Defensa: ${rJSON.stats[3].base_stat}`)
-    $("#ataque-s").text(`Ataque S: ${rJSON.stats[2].base_stat}`)
-    $("#defensa-s").text(`Defensa S: ${rJSON.stats[1].base_stat}`)
-    $("#velocidad").text(`Velocidad: ${rJSON.stats[0].base_stat}`)
-    $("#vitalidad").text(`Vitalidad: ${rJSON.stats[5].base_stat}`)
-}
-
-
-
-
-async function obtenerDescripcion(id) {
-    const r = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}/`)
-    const rJSON = await r.json()
-    const descripcion = rJSON.flavor_text_entries.find((x) =>
-        x.language.name === "es");
-    $("#descripcion").text(`${descripcion.flavor_text}`)
-}
-
-async function obtenerHabilidad(name) {
-    let array = []
-    const URL = `https://pokeapi.co/api/v2/pokemon/${name}/`
-    const r = await fetch(URL)
-    const rJSON = await r.json()
-
-    rJSON.abilities.forEach(elemento => {
-        array.push(elemento.ability.url)
-    })
-    console.log(array)
-
-
-    return array
+function obtenerNumeroPokemon(elemento) {
+    let numeroPokemon = elemento.substr(elemento.length - 5)
+    return numeroPokemon.replace(/[^0-9]/g, '');
 }
 
 async function traducirEspaniol(elemento) {
@@ -183,13 +188,4 @@ async function traducirEspaniol(elemento) {
     }
     return traduccionArray
 
-}
-
-$("#boton-ingresar-pokemon").click(() => {
-    const pokemon = $("#ingresar-pokemon").val()
-    buscarPokemon(pokemon)
-})
-
-function buscarPokemon(pokemon) {
-    mostrarPokemonSeleccionado(pokemon)
 }
---
+++ src/ui/mostrar-carta.js
@@ -0,0 +1,103 @@
+import {
+    agregarCeros,
+    primerLetraMayus,
+    traducirEspaniol
+} from "../utilidades.js"
+
+
+export async function mostrarPokemonSeleccionado(pokemon) {
+    const URL = (`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+    const r = await fetch(URL)
+    const rJSON = await r.json()
+
+    mostrarMiniDescripcion(rJSON)
+    mostrarNumeroYNombrePokemon(rJSON)
+    mostrarImagenPokemon(rJSON)
+    mostrarStatsPokemon(rJSON)
+    mostrarDescripcion(rJSON)
+    mostrarTipo(rJSON)
+    manejarHabilidades(rJSON)
+}
+async function manejarHabilidades(rJSON) {
+    debugger
+    const habilidades = await obtenerHabilidad(rJSON)
+    const habilidadesEspaniol = await traducirEspaniol(habilidades)
+    mostrarHabilidad(habilidadesEspaniol)
+}
+
+function mostrarTipo(rJSON) {
+    $("#tipos").html("")
+    rJSON.types.forEach(types => {
+        $("#tipos").append(`<img src="img/Tipos/${types.type.name}.gif"/>`)
+    })
+}
+
+async function mostrarMiniDescripcion(elemento) {
+    const r = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${elemento.id}/`)
+    const rJSON = await r.json()
+    let miniDescripcion = rJSON.genera.find((x) =>
+        x.language.name === "es");
+    $("#mini-descripcion").text(miniDescripcion.genus)
+}
+
+function mostrarNumeroYNombrePokemon(rJSON) {
+    $("#id-pokemon").text(`Nº ${rJSON.id} ${primerLetraMayus(rJSON.name)}`)
+}
+
+function obtenerImagenPokemon(id) {
+    const URLFoto = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${agregarCeros(id, 3)}.png`
+    return URLFoto;
+}
+
+function mostrarImagenPokemon(rJSON) {
+    $("#imagen-pokemon").attr("src", `${obtenerImagenPokemon(rJSON.id)}`)
+}
+
+function mostrarStatsPokemon(rJSON) {
+    $("#ataque").text(`Ataque: ${rJSON.stats[4].base_stat}`)
+    $("#defensa").text(`Defensa: ${rJSON.stats[3].base_stat}`)
+    $("#ataque-s").text(`Ataque S: ${rJSON.stats[2].base_stat}`)
+    $("#defensa-s").text(`Defensa S: ${rJSON.stats[1].base_stat}`)
+    $("#velocidad").text(`Velocidad: ${rJSON.stats[0].base_stat}`)
+    $("#vitalidad").text(`Vitalidad: ${rJSON.stats[5].base_stat}`)
+}
+
+async function mostrarDescripcion(elemento) {
+    const r = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${elemento.id}/`)
+    const rJSON = await r.json()
+    const descripcion = rJSON.flavor_text_entries.find((x) =>
+        x.language.name === "es");
+    $("#descripcion").text(`${descripcion.flavor_text}`)
+}
+
+async function obtenerHabilidad(elemento) {
+    let array = []
+    const URL = `https://pokeapi.co/api/v2/pokemon/${elemento.name}/`
+    const r = await fetch(URL)
+    const rJSON = await r.json()
+
+    rJSON.abilities.forEach(elemento => {
+        array.push(elemento.ability.url)
+    })
+    return array
+}
+
+function mostrarHabilidad(array) {
+    $("#habilidad").html("")
+
+    array.forEach(elemento => {
+        $("#habilidad").append(`<div>${elemento}`)
+    })
+}
+
+export function listaClickeable() {
+    debugger
+    $("tr").click(() => {
+        let click = clickPokemonLista();
+        mostrarPokemonSeleccionado(click);
+    });
+}
+function clickPokemonLista() {
+    const click = event.target
+    return click.dataset.pokemon
+}
